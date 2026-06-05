92a1f2a2916bca07e54991ead3c5263d49699174
Leannaentries1 <privateedition26@gmail.com>
2026-06-05 13:32:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const phoneButton = document.getElementById("phoneButton");
+const flipPhone = document.getElementById("flipPhone");
 
+phoneButton.addEventListener("click", () => {
+  flipPhone.classList.toggle("open");
+});
+
+function vibratePhone() {
+  phoneButton.classList.add("vibrate");
+
+  setTimeout(() => {
+    phoneButton.classList.remove("vibrate");
+  }, 1200);
+}
+
+setTimeout(vibratePhone, 1800);
